60d3f3c8bb6aeb01f4ce1f4cfcce4d5eed6a7e53
RANJEET <ranjeetv1106@gmail.com>
2026-08-15 01:50:08 +0530

    updated test profile

--- backend/src/test/java/com/quickshare/quickshare/QuickShareApplicationTests.java
+++ backend/src/test/java/com/quickshare/quickshare/QuickShareApplicationTests.java
@@ -2,8 +2,10 @@ package com.quickshare.quickshare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class QuickShareApplicationTests {
 
 	@Test
